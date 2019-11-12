1f246e9940b0d4bd308b340ee204bd9432e9485c
Nemo-Nowane <54185653+Nemo-Nowane@users.noreply.github.com>
2019-11-12 23:43:14 +0600

    Update quadratic.py

--- quadratic.py
+++ quadratic.py
@@ -3,7 +3,7 @@ import sys #For getting input from CLI
 def qud(arg1, arg2, arg3): #Defining the function
     x1 = (-arg2 + cmath.sqrt(arg2**2 - 4*arg1*arg3))/(2*arg1) 
     x2 = (-arg2 - cmath.sqrt(arg2**2 - 4*arg1*arg3))/(2*arg1)
-    return x1, x2 g
+    return x1, x2 
 print(qud(*map(float, sys.argv[1:])))
 """ All in one line, first transforming all argument vectors in float, passing them through 
 the defined function and printing the outputs. I'm a bit lazy. :) """
