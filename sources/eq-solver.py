ab58337b10947bbb761bb6be79e3b1b4025dabe2
Nemo-Nowane <54185653+Nemo-Nowane@users.noreply.github.com>
2019-11-14 20:26:27 +0600

    Add files via upload

--- sources/eq-solver.py
+++ sources/eq-solver.py
@@ -1,7 +1,7 @@
-from fractions import Fraction #To implement algorithm without messing floats
+from fractions import Fraction
 from decimal import Decimal
-import sys#For CLI input
-def factor(arg1):#Factor finding function
+import sys
+def factor(arg1):
    l = [1]
    a = 2
    while a <= (abs(arg1))**.5:
@@ -9,39 +9,39 @@ def factor(arg1):#Factor finding function
          l.append(a)
       a = a + 1
    return l
-def plot (o, d, t, dlist):#Table maker function, for comments goto table-maker.py
+def plot (o, d, t, dlist):
    y = 0
    xlist=[]
    fxlist=[]
    r = o
-   while o<= r and r<= d: 
-      for j in range(len(dlist)):
+   while o<= r and r<= d: #Putting all the values in function of x
+      for j in range(len(dlist)):#"Defining" the function of x in r
          y = dlist[j]*r**(len(dlist) -1 -j) + y
          xlist.append(r)
          fxlist.append(y)
-         y = 0 
-         r = float(Decimal(r) + Decimal(t))
+         y = 0 #This stops the previous value of f(x) to be added in sub>
+         r = float(Decimal(r) + Decimal(t)) #Decimal function is used to>
    return xlist, fxlist
-def mark(lm, qlist):#Table maker function modified for finding point
+def mark(lm, qlist):
    kj = 0
    for re in range(len(qlist)):
       kj = qlist[re]*lm**(len(qlist) -1 -re) + kj
    return kj
-inlist = list(map(float,sys.argv[1:]))#List of coefficients 
+inlist = list(map(float, input("Values of coefficient").split()))
 zlist =[]
-a = inlist[0]#Leading coefficient 
+a = inlist[0]
 t =-1
-while inlist[t] == 0:#Constant or coefficient of smallest term
+while inlist[t] == 0:
     t = t-1
     zlist.append(0)
 c = inlist[t]  
 n = 2
-alist =[*factor(a), *list(map(lambda x: x*(-1), (factor(a))))]#Factors of leading coefficient 
-clist =[*factor(c), *list(map(lambda x: x*(-1), (factor(c))))]#Factors of constant or smallest coefficient 
-for w in range (len(clist)): #Running the algorithm, the algorithm is described in README.md
+alist =[*factor(a), *list(map(lambda x: x*(-1), (factor(a))))]
+clist =[*factor(c), *list(map(lambda x: x*(-1), (factor(c))))]
+for w in range (len(clist)):
    for q in range (len(alist)):
        z =  Fraction(clist[w], alist[q])
        if mark(float(z), inlist ) ==0:
          zlist.append(z)
          
-print(list(set(map(float,(zlist)))))#Getting the result as set, each domain once.
+print(list(set(map(float,(zlist)))))
